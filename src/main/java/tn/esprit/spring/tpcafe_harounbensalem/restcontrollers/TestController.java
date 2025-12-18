c47d8c8e543d8e94dbd8062fc921f2a4b3ac0095
haroun ben salem <haroun.bensalem@esprit.tn>
2025-12-18 14:22:19 +0100

    spring kubernetes

--- src/main/java/tn/esprit/spring/tpcafe_harounbensalem/restcontrollers/TestController.java
+++ src/main/java/tn/esprit/spring/tpcafe_harounbensalem/restcontrollers/TestController.java
@@ -1,2 +1,12 @@
-package tn.esprit.spring.tpcafe_harounbensalem.restcontrollers;public class TestController {
+package tn.esprit.spring.tpcafe_harounbensalem.restcontrollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot fonctionne sur Kubernetes 🚀";
+    }
 }
